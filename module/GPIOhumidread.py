09e70b929cb3470d493361cad026f76855017013
SunKyu <leesunkeuy@naver.com>
2014-07-07 01:53:58 +0000

    hello

--- module/GPIOhumidread.py
+++ module/GPIOhumidread.py
@@ -4,7 +4,7 @@ import time
 def GPIOhumidRead():
   sensor =  Adafruit_DHT.DHT11
   pin = "P9_11"
-
+  print "read humid "
   humid, temp = Adafruit_DHT.read_retry(sensor, pin)
-
+  print "after read humid"
   return humid
--- module/GPIOtemperread.py
+++ module/GPIOtemperread.py
@@ -4,7 +4,7 @@ import time
 def GPIOtemperRead():
   sensor =  Adafruit_DHT.DHT11
   pin = "P9_11"
-
+  print "before read temper"
   humid, temp = Adafruit_DHT.read_retry(sensor, pin)
-
+  print "after read temper"
   return temp
