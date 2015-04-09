9a1ad9fd955bc4fa58a8d9e8d6bc488a1929ddb2
SunKyu Lee <leesunkeuy@naver.com>
2015-04-09 20:25:15 +0900

    module

---
+++ module/clientmodule.py
@@ -0,0 +1,26 @@
+# file: l2capclient.py
+# desc: Demo L2CAP client for bluetooth module.
+# $Id: l2capclient.py 524 2007-08-15 04:04:52Z albert $
+
+import sys
+import bluetooth
+import time
+def clientmodule(message, addr):
+  time.sleep(2.5)
+  if sys.version < '3':
+    input = raw_input
+
+  sock=bluetooth.BluetoothSocket(bluetooth.L2CAP)
+  bt_addr=addr
+  print bt_addr
+  port = 0x1001
+
+  print("trying to connect to %s on PSM 0x%X" % (bt_addr, port))
+
+  sock.connect((bt_addr, port))
+
+  print("connected.  type stuff")
+  sock.send(message)
+  print("send message : %s "%message)
+  sock.close()
+
---
+++ module/servermodule.py
@@ -0,0 +1,18 @@
+import bluetooth
+
+def servermodule ():
+  
+  server_sock=bluetooth.BluetoothSocket( bluetooth.L2CAP )
+
+  port = 0x1001
+
+  server_sock.bind(("",port))
+  server_sock.listen(1)
+
+  client_sock,address = server_sock.accept()
+  print("Accepted connection from ",address)
+
+  data = client_sock.recv(1024)
+  print("Data received:", data)
+  recive = "%s/%s" %(data, str(address[0]))
+  return recive
