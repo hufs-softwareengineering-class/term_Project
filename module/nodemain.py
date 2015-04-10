6162edcef8504075e60839885c2f25fcb9d2990c
SunKyu Lee <leesunkeuy@naver.com>
2015-04-10 18:57:18 +0900

    add the main.py

---
+++ module/nodemain.py
@@ -0,0 +1,25 @@
+import Node
+import sys
+import time
+from test_inquiry import *
+from servermodule import *
+from clientmodule import *
+
+'''
+schemas = {
+    "search" : search,
+    "searcheres" : searchres,
+    "get" : get,
+    "getres" : getres,
+    "put" : put
+    }
+'''
+node = Node()
+
+while 1:
+  data = servermodule()
+  dataparse = data.split('/')
+  schema = dataparse[0]
+  address = dataparse[len(dataparse)-1]
+  node.schema(dataparse, address)
+
