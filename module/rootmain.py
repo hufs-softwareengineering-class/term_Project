16acaacb0c8faec79e098070d20293fb2bcff22a
SunKyu Lee <leesunkeuy@naver.com>
2015-05-02 00:57:07 +0900

    make skeleton main

--- module/rootmain.py
+++ module/rootmain.py
@@ -1,7 +1,25 @@
 from Root import *
-
+import os, time, sys, Queue
 
 
 if __name__ == "__main__":
+
+
+  # Make Queeue
+  queue = Queue()
+  root = Root()
+  root.makeDAG()
+
+
+  #need to add the pipe module
+
+
+  if ~pipe.empty(): # need to modify
+    queue.put(text)
+  else if pipe.empty(): #need to modify
+    root.getData()
+  else :
+    text = queue.get()
+
 
 
