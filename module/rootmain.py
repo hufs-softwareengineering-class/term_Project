a16ab470d85e4de7dc1441ba1ee7493bbc10f9c6
SunKyu Lee <leesunkeuy@naver.com>
2015-05-03 13:09:19 +0900

    add some global variable

--- module/rootmain.py
+++ module/rootmain.py
@@ -1,25 +1,29 @@
 from Root import *
 import os, time, sys, Queue
 
+pipe_naem = "pipefile"
+Time = datetime.datetime.now()
+timeList = []
 
 if __name__ == "__main__":
 
+
+  pipein = os.open(pipe_name, 'r')
 
   # Make Queeue
   queue = Queue()
   root = Root()
   root.makeDAG()
-
-
+  
   #need to add the pipe module
-
-
-  if ~pipe.empty(): # need to modify
-    queue.put(text)
-  else if pipe.empty(): #need to modify
-    root.getData()
-  else :
-    text = queue.get()
+  while 1:
+    line = pipein.readline()[:-1]
+    if ~pipe.empty(): # need to modify
+      queue.put(line)
+    else if pipe.empty(): #need to modify
+      root.getData()
+    else :
+      text = queue.get()
 
 
 
