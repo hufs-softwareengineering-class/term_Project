caaf9f883dae5cceb967ef5cb343ddd49183a7de
SunKyu Lee <leesunkeuy@naver.com>
2015-05-13 19:26:00 +0900

    add sql script

---
+++ module/schema.sql
@@ -0,0 +1,76 @@
+create table light (
+  room1 integer
+  room2 integer
+  room3 integer
+  room5 integer
+  room6 integer
+  room7 integer
+  room8 integer
+  room9 integer
+  room10 integer
+  room11 integer
+  room12 integer
+  room13 integer
+  room14 integer
+  room15 integer
+  room16 integer
+  room17 integer
+  room18 integer
+  room19 integer
+  room20 integer
+);
+create table temperature (
+  room1 integer
+  room2 integer
+  room3 integer
+  room5 integer
+  room6 integer
+  room7 integer
+  room8 integer
+  room9 integer
+  room10 integer
+  room11 integer
+  room12 integer
+  room13 integer
+  room14 integer
+  room15 integer
+  room16 integer
+  room17 integer
+  room18 integer
+  room19 integer
+  room20 integer
+);
+create table humid (
+  room1 integer
+  room2 integer
+  room3 integer
+  room5 integer
+  room6 integer
+  room7 integer
+  room8 integer
+  room9 integer
+  room10 integer
+  room11 integer
+  room12 integer
+  room13 integer
+  room14 integer
+  room15 integer
+  room16 integer
+  room17 integer
+  room18 integer
+  room19 integer
+  room20 integer
+);
+
+
+create table if not exists standard_Setting(
+  high integer
+  low integer
+
+);
+
+
+create table if not exists person_count(
+  count integer
+
+  );
