5a5dc314894b8c9839278ec397d44ceeba26661b
nasang <sktkddusrj@naver.com>
2015-05-21 13:33:21 +0900

    add cal temp

--- module/GPIOtemperread.py
+++ module/GPIOtemperread.py
@@ -2,4 +2,4 @@ import Adafuit_BBIO.ADC as ADC
 
 def GPIOtemperRead():
   ADC.setup()
-  return ADC.read("  ") #fill the blank port num and pinnum
+  return ( ADC.read("p8_14") - 500 )/10 #fill the blank port num and pinnum
