7ed08f1642be30d1bb71a9c5310ca41acc5d2544
SunKyu Lee <leesunkeuy@naver.com>
2015-06-09 04:07:55 +0900

    test inev

--- module/minitwit.py
+++ module/minitwit.py
@@ -14,7 +14,6 @@ from werkzeug.security import check_password_hash, generate_password_hash
 #pipe
 pipe_name = 'pipefile'
 pipe_name2 = 'pipefile2'
-inve = 0 #침입감지
 if not os.path.exists(pipe_name):
 	os.mkfifo(pipe_name)
 
@@ -72,6 +71,7 @@ def connect_db():
 
 def init_db():
     """Creates the database tables."""
+    inve = 0 #침입감지
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
