f00980bf03d58fedcdc917ac9ba280946687c41b
SunKyu <leesunkeuy@naver.com>
2015-06-08 19:26:55 +0900

    add pipeprocess

---
+++ module/pipeprocess.py
@@ -0,0 +1,13 @@
+import os, time, sys
+
+def pipeprocess(ipcque):
+  pipe_name = 'pipefile'
+  if not os.path.exists(pipe_name):
+    os.mkfifo(pipe_name)
+
+  pipein = open(pipe_name , 'r')
+
+  while True:
+    line = pipein.readline()[:-1]
+    print "pipe got message from web"
+    ipcque.put(line)
